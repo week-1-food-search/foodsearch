e0ffd6ad49764453cbc2549245365f4f6bfc44d1
yploekito <yp.loekito@gmail.com>
2019-06-13 17:43:48 +0700

    intial commit

---
+++ server/app.js
@@ -0,0 +1,18 @@
+if(!process.env.NODE_ENV || process.env.NODE_ENV === 'development' ){
+    require('dotenv').config()
+}
+
+const express = require('express')
+const app = express()
+const port = 3000 
+const index=require('./routers/index.js')
+const cors= require('cors')
+
+app.use(cors())
+
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+app.use('/', index)
+
+app.listen(port, ()=>console.log('listening to port :', port))    
