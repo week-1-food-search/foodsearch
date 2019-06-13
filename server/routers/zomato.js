151279b110f0934abf4bc4bea5320d7f1293e3f7
yploekito <yp.loekito@gmail.com>
2019-06-13 21:37:02 +0700

    done

--- server/routers/zomato.js
+++ server/routers/zomato.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const zomatoController = require('../controller/zomato')
+const zomatoController = require('../controllers/zomato')
 
 router.get('/restaurants/:food', zomatoController.getRestaurants)
 router.get('/restaurants/detail/:id', zomatoController.getRestaurantDetail)
