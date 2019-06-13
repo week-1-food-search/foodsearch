8cc9eee5f3ed8f7fcb59cce7e300b971a6ae4a4a
alvinchristian7 <alvin.christian7@yahoo.com>
2019-06-13 17:54:04 +0700

    fix typo npm i

---
+++ server/routers/food2fork.js
@@ -0,0 +1,7 @@
+const router = require('express').Router()
+const food2fork = require('../controllers/food2fork')
+
+router.get('/artist', food2fork.searchByArtist)
+router.get('/album/:id', food2fork.albumDetail)
+
+module.exports = router
